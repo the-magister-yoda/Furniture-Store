7f51d0be6dd53943cf1fbd65162baba5dd73996e
the-magister-yoda <example@mail.com>
2025-11-06 22:45:36 +0200

    saving before adding to github

--- goods/views.py
+++ goods/views.py
@@ -2,17 +2,31 @@ from django.core.paginator import Paginator
 from django.shortcuts import render, get_list_or_404
 
 from goods.models import Products
+from goods.utils import q_search
 
 
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug=None):
+
+    page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
 
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
+    if on_sale:
+        goods = goods.filter(discount__gt=0)
+
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by)
+
     paginator = Paginator(goods, 3)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
 
     context = {
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from goods.models import Categories
 
